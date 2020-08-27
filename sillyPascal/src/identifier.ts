060d5407f10cc7fd46e509bc228fdfa8682ab76b
Aloysio Rabello <aloysio.rabello@gmail.com>
2020-08-27 19:21:14 -0300

    feature: add a silly pascal identifier code.

---
+++ sillyPascal/src/identifier.ts
@@ -0,0 +1,15 @@
+export function isValid (identifier: string): boolean {
+  if (identifier === null) {
+    return false
+  }
+  if (identifier.length < 1 || identifier.length > 6) {
+    return false
+  }
+  if (!/^[a-zA-Z]+$/.test(identifier.charAt(0))) {
+    return false
+  }
+  if (!/^[0-9a-zA-Z]+$/.test(identifier)) {
+    return false
+  }
+  return true
+}
