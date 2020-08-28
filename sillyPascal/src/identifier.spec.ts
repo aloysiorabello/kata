f8b77fbef548859bf2adf88b58cc54c41d36f0b9
Aloysio Rabello <aloysio.rabello@gmail.com>
2020-08-28 00:30:06 -0300

    Update identifier.spec.ts

--- sillyPascal/src/identifier.spec.ts
+++ sillyPascal/src/identifier.spec.ts
@@ -6,37 +6,37 @@ describe('identifier validator', () => {
     expect(ret).toEqual(true)
   })
 
-  test('Should`t classify number start string as a silly parcal identifier', () => {
+  test('Shouldn`t classify number start string as a silly parcal identifier:', () => {
     const ret = isValid('1234')
     expect(ret).toEqual(false)
   })
 
-  test('Should`t classify a special caracter as a silly pascal identifier', () => {
+  test('Shouldn`t classify a special caracter String as a silly pascal identifier:', () => {
     const ret = isValid('a12^34')
     expect(ret).toEqual(false)
   })
 
-  test('Should correctly classify something', () => {
+  test('Shouldn`t classify a empty String as a silly pascal identifier:', () => {
     const ret = isValid('')
     expect(ret).toEqual(false)
   })
 
-  test('Big string', () => {
+  test('Shouldn`t classify a big String as a silly pascal identifier:', () => {
     const ret = isValid('a1234567a')
     expect(ret).toEqual(false)
   })
 
-  test('null', () => {
+  test('Shouldn`t classify a null String as a silly pascal identifier:', () => {
     const ret = isValid(null)
     expect(ret).toEqual(false)
   })
 
-  test('tam 01', () => {
+  test('Should correctly classify one char string as valid silly pascal identifier:', () => {
     const ret = isValid('a')
     expect(ret).toEqual(true)
   })
 
-  test('tam 06', () => {
+  test('Should correctly classify a six char string as valid silly pascal identifier:', () => {
     const ret = isValid('a12345')
     expect(ret).toEqual(true)
   })
